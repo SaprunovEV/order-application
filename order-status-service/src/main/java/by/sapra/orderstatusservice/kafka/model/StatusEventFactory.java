ea04a70d7d33008cea1fddd60090c49bf6d9fc18
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-14 13:32:27 +0300

    Добавлена забытая аннотация.

--- order-status-service/src/main/java/by/sapra/orderstatusservice/kafka/model/StatusEventFactory.java
+++ order-status-service/src/main/java/by/sapra/orderstatusservice/kafka/model/StatusEventFactory.java
@@ -1,7 +1,10 @@
 package by.sapra.orderstatusservice.kafka.model;
 
+import org.springframework.stereotype.Component;
+
 import java.time.Instant;
 
+@Component
 public class StatusEventFactory {
     public StatusEvent create(OrderEvent orderEvent) {
         StatusEvent statusEvent = new StatusEvent();
