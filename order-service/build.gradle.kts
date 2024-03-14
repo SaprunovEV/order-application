468d875eb1ee2d63b5ab5a7a96a13993cd478343
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-14 13:43:08 +0300

    Добавлены зависимости для Kafka и асинхронного тестирования.

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -23,9 +23,19 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.kafka:spring-kafka")
+
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:kafka")
+
+	testImplementation("org.awaitility:awaitility:4.2.0")
 }
 
 tasks.withType<Test> {
