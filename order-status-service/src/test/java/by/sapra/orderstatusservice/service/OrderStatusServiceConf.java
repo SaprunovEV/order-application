eef8988dd9cf97fb2c92c7a8286960547badb218
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-14 13:32:00 +0300

    переход на новую парадигму KafkaTemplate.

--- order-status-service/src/test/java/by/sapra/orderstatusservice/service/OrderStatusServiceConf.java
+++ order-status-service/src/test/java/by/sapra/orderstatusservice/service/OrderStatusServiceConf.java
@@ -1,6 +1,5 @@
 package by.sapra.orderstatusservice.service;
 
-import by.sapra.orderstatusservice.kafka.model.StatusEvent;
 import by.sapra.orderstatusservice.kafka.model.StatusEventFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +8,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 @TestConfiguration
 public class OrderStatusServiceConf {
     @Bean
-    public OrderStatusService service(StatusEventFactory factory, KafkaTemplate<String, StatusEvent> kafka) {
+    public OrderStatusService service(StatusEventFactory factory, KafkaTemplate<String, Object> kafka) {
         return new KafkaOrderStatusService(factory, kafka);
     }
 }
