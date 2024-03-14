1f3417c207bbeb1641fffac22b1b3e5a2330f69f
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-14 13:36:39 +0300

    Разработан слушатель для событий в Kafka.

---
+++ order-status-service/src/main/java/by/sapra/orderstatusservice/kafka/listener/KafkaOrderListener.java
@@ -0,0 +1,33 @@
+package by.sapra.orderstatusservice.kafka.listener;
+
+import by.sapra.orderstatusservice.kafka.model.OrderEvent;
+import by.sapra.orderstatusservice.service.OrderStatusService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class KafkaOrderListener {
+    private final OrderStatusService service;
+
+    public KafkaOrderListener(OrderStatusService service) {
+        this.service = service;
+    }
+
+    @KafkaListener(
+            topics = "${app.kafka.read-topic}",
+            groupId = "${app.kafka.group-id}",
+            containerFactory = "concurrentKafkaListenerContainerFactory"
+    )
+    public void handleOrderEvent(
+            @Payload OrderEvent orderEvent,
+            @Header(value = KafkaHeaders.RECEIVED_KEY, required = false) String key
+    ) {
+        log.info("Event: {}, Key: {}", orderEvent, key);
+        service.processEvent(key, orderEvent);
+    }
+}
